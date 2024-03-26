b073b67646f88e285134d08fb260d55d943e4e94
Isaiah <isaiah.ekue@protonmail.com>
2024-03-26 23:05:32 +0100

    added css files for slideshow

--- src/Components/Slides.jsx
+++ src/Components/Slides.jsx
@@ -1,16 +1,19 @@
 
-import locationList from "../routes/logements.json"
-
-
+import locationList from "../routes/logements.json";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
+import { faAngleLeft } from "@fortawesome/free-solid-svg-icons";
 export default function TheSlides() {
 
     const coverPage = locationList[0].cover;
     console.log(coverPage);
     return(
         <>
-        <h1>Test</h1>
+        <div className="slideShow">
         <img src={coverPage} alt="" />
-        
+        <FontAwesomeIcon icon={faAngleLeft} />
+        <FontAwesomeIcon icon={faAngleRight} />
+        </div>
       </>
     )
  
