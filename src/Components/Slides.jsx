267c86ed2260bf840744c65d300a040db2d99585
Isaiah <isaiah.ekue@protonmail.com>
2024-03-26 01:17:49 +0100

    first try to add data from json file

--- src/Components/Slides.jsx
+++ src/Components/Slides.jsx
@@ -1,9 +1,17 @@
-import "../routes/logements.json";
 
-const firstPicture = pictures[0];
+import locationList from "../routes/logements.json"
+
+
 export default function TheSlides() {
-  <>
-    <img src={firstPicture} alt="" />
-    <FontAwesomeIcon icon="fa-regular fa-angle-right" />
-  </>;
+
+    const coverPage = locationList[0].cover;
+    console.log(coverPage);
+    return(
+        <>
+        <h1>Test</h1>
+        <img src={coverPage} alt="" />
+        
+      </>
+    )
+ 
 }
--- src/routes/location.jsx
+++ src/routes/location.jsx
@@ -1,13 +1,13 @@
 import TheHeader from "../Components/header";
 import TheFooter from "../Components/footer";
-
+import TheSlides from "../Components/Slides";
 export default function LocationPage() {
   return (
     <>
       <TheHeader />
       
         <main>
-
+        <TheSlides />
         </main>
       
       <footer>
